931ac3fcbd8973a3f7a2e824f254f7fed75c1394
Noah Soliday Bench <nbench0218@gmail.com>
2022-12-04 20:53:54 -0800

    day 4

---
+++ day_04/a_main.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Noah Soliday Bench
+# Made for Advent of Code 2022
+
+import os
+from utils import input_utils
+
+# get our input
+script_path = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(script_path, "input.txt")
+input_data = input_utils.read_split(data_path)
+
+
+fully_contained_count = 0
+
+for pair in input_data:
+    elf_a = set(range(int(pair.split(',')[0].split('-')[0]), int(pair.split(',')[0].split('-')[1]) + 1))
+    elf_b = set(range(int(pair.split(',')[1].split('-')[0]), int(pair.split(',')[1].split('-')[1]) + 1))
+
+    if elf_a.intersection(elf_b) == elf_a or elf_a.intersection(elf_b) == elf_b:
+        fully_contained_count += 1
+
+
+print(fully_contained_count)
